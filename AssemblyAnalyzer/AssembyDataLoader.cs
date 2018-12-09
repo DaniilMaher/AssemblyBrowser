b8cc1f2f8d207859facc819c70b5b948fc3948fa
DanilaMaher <daniilnews@gmail.com>
2018-12-09 13:01:16 +0300

    Assembly data structure defined

---
+++ AssemblyAnalyzer/AssembyDataLoader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace AssemblyAnalyzer
+{
+    public class AssembyDataLoader
+    {
+
+        public static AssemblyData LoadAssemblyData(string path)
+        {
+            Assembly assembly = Assembly.LoadFrom(path);
+            return new AssemblyData(assembly);
+        }
+        
+    }
+}
