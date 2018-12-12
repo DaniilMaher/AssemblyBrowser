609205f1639da95669de30c2031bcf18f60b836e
DanilaMaher <daniilnews@gmail.com>
2018-12-12 19:08:42 +0300

    UnitTests added

---
+++ AssemblyAnalyzerUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AssemblyAnalyzerUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssemblyAnalyzerUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("62589d59-07c1-49b6-b426-8f3072ef6a74")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AssemblyAnalyzerUnitTest/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AssemblyAnalyzer;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace AssemblyAnalyzerUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        AssemblyData assemblyData;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            string path = @"..\..\TestDLL\Faker.dll";
+            Assembly assembly = Assembly.LoadFrom(path);
+            assemblyData = new AssemblyData(assembly);
+        }
+
+        [TestMethod]
+        public void TestNamespaceCount()
+        {
+            Assert.AreEqual(assemblyData.Namespaces.Count, 3);
+        }
+
+        [TestMethod]
+        public void TestNamespaceName()
+        {
+            Assert.IsNotNull(assemblyData.Namespaces.Find(n => n.Name == "Faker.BaseTypesValuesGenerators"));
+        }
+        
+        [TestMethod]
+        public void TestClassesCount()
+        {
+            Namespace ns = assemblyData.Namespaces.Find(n => n.Name == "Faker");
+            Assert.AreEqual(ns.Types.Count, 7);
+        }
+
+        [TestMethod]
+        public void ClassName()
+        {
+            Namespace ns = assemblyData.Namespaces.Find(n => n.Name == "Faker");
+            Assert.IsNotNull(ns.Types.Find(t => t.Name == "GeneratorsDictionaryCreator"));
+        }
+
+        [TestMethod]
+        public void TypeMethodsCount()
+        {
+            Namespace ns = assemblyData.Namespaces.Find(n => n.Name == "Faker");
+            TypeInfo type = ns.Types.Find(t => t.Name == "Faker");
+            Assert.AreEqual(new List<MethodInfo>(type.DeclaredMethods).Count, 7);
+        }
+
+        [TestMethod]
+        public void TypeFieldsCount()
+        {
+            Namespace ns = assemblyData.Namespaces.Find(n => n.Name == "Faker");
+            TypeInfo type = ns.Types.Find(t => t.Name == "Faker");
+            Assert.AreEqual(new List<FieldInfo>(type.DeclaredFields).Count, 5);
+        }
+
+
+    }
+}
